2fcb8fb938326fdd90aedb0ec1b8e0d2bfc59edb
Thurule <github@aobrien5.com>
2024-10-28 14:24:14 -0500

    update vars

--- vpc-b/variables.tf
+++ vpc-b/variables.tf
@@ -1,13 +1,13 @@
 # security credential variables
-variable "access_key" {
-  type        = string
-  sensitive   = true
-}
+#variable "access_key" {
+#  type        = string
+#  sensitive   = true
+#}
 
-variable "secret_key" {
-  type        = string
-  sensitive   = true
-}
+#variable "secret_key" {
+#  type        = string
+#  sensitive   = true
+#}
 
 variable "region" {
   type        = string
