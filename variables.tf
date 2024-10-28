e05dd853c8d3b832353e58dc5b42b85ee9906a68
Thurule <github@aobrien5.com>
2024-10-28 11:02:29 -0500

    update profile and access keys

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
-variable "access_key" {
-  type        = string
-  sensitive   = true
-}
+#variable "access_key" {
+#  type        = string
+#  sensitive   = true
+#}
 
-variable "secret_key" {
-  type        = string
-  sensitive   = true
-}
+#variable "secret_key" {
+#  type        = string
+#  sensitive   = true
+#}
 
 variable "region" {
   type        = string
